b3ec072b0e9471a106564e980ef8c46a365e4b55
khmerak <141410660+khmerak@users.noreply.github.com>
2025-06-14 16:05:50 +0700

    d

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,a.khmercleaningservice.us/,e.khmercleaningservice.us/,::1',
+        'localhost,localhost:3000,127.0.0.1,a.khmercleaningservice.us/,e.khmercleaningservice.us/,::1',
         Sanctum::currentApplicationUrlWithPort(),
         // Sanctum::currentRequestHost(),
     ))),
