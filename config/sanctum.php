b51c8e708b280651cab90ab94b37035eac03992d
khmerak <141410660+khmerak@users.noreply.github.com>
2025-06-14 15:49:49 +0700

    i

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,8 +15,12 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,a.khmercleaningservice.us')),
-
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
+        '%s%s',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,a.khmercleaningservice.us/,e.khmercleaningservice.us/,::1',
+        Sanctum::currentApplicationUrlWithPort(),
+        // Sanctum::currentRequestHost(),
+    ))),
 
     /*
     |--------------------------------------------------------------------------
