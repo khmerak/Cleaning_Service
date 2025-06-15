a1f5e09f9400166005b9ae3ed8390e85c02fcfd7
khmerak <141410660+khmerak@users.noreply.github.com>
2025-06-15 10:25:24 +0700

    h

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,127.0.0.1,a.khmercleaningservice.us')),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,127.0.0.1,127.0.0.1:8000/,a.khmercleaningservice.us')),
 
 
 
